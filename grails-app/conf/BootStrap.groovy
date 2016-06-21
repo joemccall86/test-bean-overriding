d6cc5dbe85a1e43c1e45c19e403e13a2102bc921
Joe McCall <joemccall@intrepid-networks.com>
2016-06-21 12:43:10 -0400

    Add assertion in bootstrap to fail fast

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,11 @@
+import xyz.joemccall.MyTokenStore
+
 class BootStrap {
 
+    def grailsApplication
+
     def init = { servletContext ->
+        assert grailsApplication.mainContext.getBean(MyTokenStore) != null, 'MyTokenStore was not injected!'
     }
     def destroy = {
     }
